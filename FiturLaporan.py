c0d0ad209879131400658c9584c032d7252add29
ghaniathar-sketch <ghaniathar@apps.ipb.ac.id>
2026-09-20 22:40:14 +0700

    Perubahan status laporan Branch A

--- FiturLaporan.py
+++ FiturLaporan.py
@@ -3,7 +3,7 @@
 def cetak_laporan(total_panen):
     print("=== LAPORAN HASIL PANEN DIGITAL ===")
     print(f"Total Hasil Panen: {total_panen} kg")
-    print("Status: Laporan Berhasil Dicetak")
+    print("Status: Laporan siap diproses")
 
 if __name__ == "__main__":
     cetak_laporan(150)
